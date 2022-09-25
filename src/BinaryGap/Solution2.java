092db98e0e258cc34853f7ed77a9027eef4be32d
alexk <alexkpxb@gmail.com>
2022-09-25 17:04:49 -0400

    new solution

---
+++ src/BinaryGap/Solution2.java
@@ -0,0 +1,39 @@
+package BinaryGap;
+
+public class Solution2 {
+
+    public int solution(int N) {
+
+        String bin = Integer.toBinaryString(N);
+        System.out.println(bin);
+        int max_gap = 0;
+        int current_gap =0;
+
+        for (int i = 0; i < bin.length(); i++) {
+            if (bin.charAt(i) == '0') {
+                current_gap++;
+            } else {
+                if (current_gap > 0) {
+                    max_gap = Math.max(max_gap, current_gap);
+                    current_gap = 0;
+                }
+            }
+        }
+        return max_gap;
+    }
+
+    public static void main(String[] args) {
+        int result = new Solution2().solution(997);
+        System.out.println("Result is = " + result);
+        result = new Solution2().solution(9);
+        System.out.println("Result is = " + result);
+        result = new Solution2().solution(529);
+        System.out.println("Result is = " + result);
+        result = new Solution2().solution(20);
+        System.out.println("Result is = " + result);
+        result = new Solution2().solution(15);
+        System.out.println("Result is = " + result);
+        result = new Solution2().solution(32);
+        System.out.println("Result is = " + result);
+    }
+}
