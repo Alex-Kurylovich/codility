c318f11bb2595309552fd75a154f85f946e7ef06
alexk <alexkpxb@gmail.com>
2022-09-26 11:51:20 -0400

    new solution

---
+++ src/OddOccurrencesInArray/Solution2.java
@@ -0,0 +1,30 @@
+package OddOccurrencesInArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution2 {
+    // It's not efficient solution as Solution.java
+    public int solution(int[] A) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            if (map.get(A[i]) != null) {
+                Integer v = map.get(A[i]);
+                map.put(A[i], ++v);
+            } else {
+                map.put(A[i], 1);
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            if (map.get(A[i]) < 2) {
+                return A[i];
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int result = new Solution2().solution(new int[] {9, 3, 9, 3, 9, 7, 9});
+        System.out.println("Result is = " + result);
+    }
+}
